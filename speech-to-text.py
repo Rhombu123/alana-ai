44f840096bcbbd656eda929eafc038b52ca845e3
Rhombu123 <149025491+Rhombu123@users.noreply.github.com>
2025-02-19 22:52:30 -0600

    Test push

---
+++ speech-to-text.py
@@ -0,0 +1,14 @@
+import speech_recognition as sr
+import pyttsx3
+
+def record_text():
+    return
+
+def output_text(text):
+    return
+
+while(1):
+    text = record_text()
+    output_text(text)
+
+    print("Wrote text")
